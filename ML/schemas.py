593dcab8b5264106cc2a4b4a76579d129227f86a
mbsurya19122004 <mbsurya19122004@gmail.com>
2026-09-08 13:16:15 +0530

    added argiculture model

--- ML/schemas.py
+++ ML/schemas.py
@@ -2,12 +2,8 @@ from pydantic import BaseModel, Field
 
 
 class CropPredictionRequest(BaseModel):
-    N: float = Field(..., description="Nitrogen content in soil")
-    P: float = Field(..., description="Phosphorus content in soil")
-    K: float = Field(..., description="Potassium content in soil")
     temperature: float
     humidity: float
-    ph: float
     rainfall: float
 
 
